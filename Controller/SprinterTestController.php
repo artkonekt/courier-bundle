3ae822cda68cdef2f8ca79c84cd7cccae796780a
Lajos Fazakas <lajos@artkonekt.com>
2016-03-07 20:40:22 +0200

    * [x] regenerate classes from wsdl, try out the generated classes instead of the provided ones * [x] integrate the generated classes into a new konekt-courier subpackage: Sprinter * [x] add test code to the courierbundle which tests the sprinter parcel creation with the new library * [x] split konekt-courier-bundle configuration into fancourier and sprinter

---
+++ Controller/SprinterTestController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Contains class SprinterTestController
+ *
+ * @package     Konekt\CourierBundle\Controller
+ * @copyright   Copyright (c) 2016 Storm Storez Srl-D
+ * @author      Lajos Fazakas <lajos@artkonekt.com>
+ * @license     Proprietary
+ * @since       2016-03-07
+ * @version     2016-03-07
+ */
+
+namespace Konekt\CourierBundle\Controller;
+
+
+use Konekt\Courier\Sprinter\PartnerToPudo\Transaction\RegisterParcel\RegisterParcelRequest;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SprinterTestController extends Controller
+{
+    public function testRegisterParcelAction(Request $request)
+    {
+        $processor = $this->get('konekt_courier.sprinter.request.processor');
+        $registerParcelRequest = new RegisterParcelRequest();
+        $response = $processor->process($registerParcelRequest)->getResponse();
+
+        var_dump($response);
+
+        // process response
+        print('ErrorCode: ' . $response->getRegisterParcelContainerResult()->ErrorCode . "\n");
+        foreach($response->getRegisterParcelContainerResult()->ParcelResults as $parcelResult) {
+            print("\tOriginalBarcode: " . $parcelResult->getOriginalBarCode() . "\n");
+            print("\tNewBarCode: " . $parcelResult->getNewBarCode() . "\n");
+            print("\tShipmentID: " . $parcelResult->getShipmentID() . "\n");
+            print("\tErrorCode: " . $parcelResult->getErrorCode() . "\n");
+            print("\n");
+        }
+
+        return new Response();
+    }
+}
